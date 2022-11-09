f9d187a7af0a4e21a401210cf15c8f60d65d3f33
Davi Lessa Vieira <davi.c.lessa@gmail.com>
2022-11-08 23:32:11 -0300

    express and router added

---
+++ app.ts
@@ -0,0 +1,11 @@
+import express, { Express } from 'express';
+import router from "./router";
+
+const app: Express = express();
+const port = 3000;
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
